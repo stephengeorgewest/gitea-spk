2e4ebb4e7a90ec4187e9692de0ee50957afd692c
Walter Hunsicker <quixote@hunsicker.de>
2017-09-05 11:33:25 +0200

    Build enhancements
     - names install packages after the binary
     - uses version number of binary for install package
     - allow to use a specific binary for building

--- create_spk.sh
+++ create_spk.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 
-chmod +x 1_create_package/gitea/gitea
-cd 1_create_package 
-tar cvfz package.tgz * 
-mv package.tgz ../2_create_project/ 
-cd ../2_create_project/ 
-tar cvfz gitea.spk * 
-mv gitea.spk ..
+if [ ! $# -eq 0 ]; then
+    if [ -f $1 ]; then
+        binary=$1
+    else
+        echo "$1 not found"
+        exit 1
+    fi
+else
+    # pick the latest binary
+    binary=$(ls -1 -t gitea-*-linux-*[!.spk] 2>/dev/null | head -1)
+
+    if [ ! $? -eq 0 ]; then
+        echo "No gitea binary found. Please download a binary from https://github.com/go-gitea/gitea/releases"
+        exit 1
+    fi
+fi
+
+version=$(echo ${binary} | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+
+# update the package meta data to match the binary version
+cp 2_create_project/INFO.in 2_create_project/INFO
+sed -i -e "s/[0-9]+\.[0-9]+\.[0-9]+/$version/" 2_create_project/INFO
+
+chmod +x $binary
+mkdir -p 1_create_package/gitea
+ln -sf "$PWD/$binary" 1_create_package/gitea/gitea
+cd 1_create_package
+tar cvfhz ../2_create_project/package.tgz *
+cd ../2_create_project/
+tar cvfz ../$binary.spk --exclude=INFO.in *
 rm -f package.tgz
+
